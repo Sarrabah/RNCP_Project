54dc2558bf353e18c95a699b58ffda3b2ae16faa
sarrabah <sarrabenarbia1996@gmail.com>
2024-08-30 16:10:40 +0200

    feat: Create a new component juste for test

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>
+                Welcome to my RNCP project
+            </h1>
+        </header>
+    )
+} 
+
+export default Header;
