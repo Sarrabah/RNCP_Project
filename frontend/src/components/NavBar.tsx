69f5a021c2b36bdfa1fb7c3620a9836d03b47d5f
sarrabah <sarrabenarbia1996@gmail.com>
2024-09-18 15:17:33 +0200

    feat: Add the navbar component

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import { ShoppingCartOutlined, UserOutlined } from '@ant-design/icons';
+import '../styles/Homepage.css';
+import artcreapro from '../assets/art-crea-pro.png';
+
+const NavBar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <img src={artcreapro} alt="Brand" className="img" />
+      <SearchBar />
+      <div className="icons">
+        <UserOutlined className="icon" />
+        <ShoppingCartOutlined className="icon" />
+      </div>
+    </div>
+  );
+};
+export default NavBar;
