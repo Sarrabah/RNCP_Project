179de3f39da9e86e70818633ae721a605891a023
sarrabah <sarrabenarbia1996@gmail.com>
2024-09-19 15:32:39 +0200

    feat: Reorganize the homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
+import { ProductProps } from '../components/Product';
+import ProductList from '../components/ProductList';
+import bouton from '../assets/bouton.png';
+import stopporte from '../assets/stop-porte.png';
+import parquet from '../assets/parquet.png';
+import robinet from '../assets/robinet.png';
 import NavBar from '../components/NavBar';
-import DropDownProducts from '../components/DropDownProducts';
-import { Divider } from 'antd';
-
+import Sidebar from '../components/SideBar';
+import { Layout } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import { Content } from 'antd/es/layout/layout';
+import '../styles/Homepage.css';
+const products: ProductProps[] = [
+  {
+    id: 1,
+    image: bouton,
+    name: 'Button',
+    price: '1.5 Euro',
+    category: 'Furniture',
+  },
+  {
+    id: 2,
+    image: stopporte,
+    name: 'Door stop',
+    price: '3 Euro',
+    category: 'Door',
+  },
+  {
+    id: 3,
+    image: parquet,
+    name: 'Parquet',
+    price: '100/m² Euro',
+    category: 'Floor',
+  },
+  {
+    id: 4,
+    image: robinet,
+    name: 'Faucet',
+    price: '129 Euro',
+    category: 'bathroom',
+  },
+];
 const Homepage: React.FC = () => {
   return (
-    <div className="homepage">
+    <Layout>
       <NavBar />
-      <Divider style={{ margin: '0', backgroundColor: '#e0e0e0' }} />
-      <DropDownProducts />
-      <Divider style={{ margin: '0', backgroundColor: '#e0e0e0' }} />
-    </div>
+      <Layout>
+        <Sider width={250}>
+          <Sidebar />
+        </Sider>
+        <Content style={{ marginTop: '2px', padding: '0 24px' }}>
+          <h2 className="title"> All our available products! </h2>
+          <ProductList products={products} />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 export default Homepage;
