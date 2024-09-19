8de64acb25b134975e69d8c0b71c17db9b4f4ee4
sarrabah <sarrabenarbia1996@gmail.com>
2024-09-19 11:29:25 +0200

    feat: Introduce the wlecome page

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../pages/LoginPage';
 import Register from '../pages/RegisterPage';
 import Homepage from '../pages/HomePage';
+import Welcomepage from '../pages/Welcomepage';
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/welcomepage" element={<Welcomepage />} />
       </Routes>
     </Router>
   );
