58c66db8778280691b07ba65771f75b8b29dfc81
sarrabah <sarrabenarbia1996@gmail.com>
2024-09-20 19:49:57 +0200

    feat: Use antd components inside ap routes instead of the home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,11 +5,7 @@ import bouton from '../assets/bouton.png';
 import stopporte from '../assets/stop-porte.png';
 import parquet from '../assets/parquet.png';
 import robinet from '../assets/robinet.png';
-import NavBar from '../components/NavBar';
-import Sidebar from '../components/SideBar';
-import { Layout } from 'antd';
-import Sider from 'antd/es/layout/Sider';
-import { Content } from 'antd/es/layout/layout';
+
 import '../styles/Homepage.css';
 const products: ProductProps[] = [
   {
@@ -44,28 +40,20 @@ const products: ProductProps[] = [
 const categories: string[] = ['Furniture', 'Door', 'Floor', 'Bathroom'];
 const Homepage: React.FC = () => {
   return (
-    <Layout>
-      <NavBar />
-      <Layout>
-        <Sider width={250}>
-          <Sidebar />
-        </Sider>
-        <Content style={{ marginTop: '2px', padding: '0 24px' }}>
-          <h2 className="title"> All our available products! </h2>
-          {categories.map((category) => {
-            const filteredProducts = products.filter(
-              (product) => product.category === category,
-            );
-            return (
-              <div key={category}>
-                <h3>{category}</h3>
-                <ProductList products={filteredProducts} />
-              </div>
-            );
-          })}
-        </Content>
-      </Layout>
-    </Layout>
+    <div>
+      <h2 className="title"> All our available products! </h2>
+      {categories.map((category) => {
+        const filteredProducts = products.filter(
+          (product) => product.category === category,
+        );
+        return (
+          <div key={category}>
+            <h3>{category}</h3>
+            <ProductList products={filteredProducts} />
+          </div>
+        );
+      })}
+    </div>
   );
 };
 export default Homepage;
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,16 +4,28 @@ import Login from '../pages/LoginPage';
 import Register from '../pages/RegisterPage';
 import Homepage from '../pages/HomePage';
 import Welcomepage from '../pages/Welcomepage';
+import NavBar from '../components/NavBar';
+import { Layout } from 'antd';
+import Sidebar from '../components/SideBar';
 
+const { Content } = Layout;
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/" element={<Welcomepage />} />
-      </Routes>
+      <Layout>
+        <NavBar />
+        <Layout>
+          <Sidebar />
+          <Content style={{ marginTop: '2px', padding: '0 24px' }}>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/homepage" element={<Homepage />} />
+              <Route path="/" element={<Welcomepage />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
