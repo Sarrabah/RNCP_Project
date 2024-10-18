863388750a9de27ad593024c9c7b1fe99a265f35
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-18 15:16:29 +0200

    feat: Add icons to be more responsive

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import {
+  HomeOutlined,
+  FormOutlined,
+  OrderedListOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons';
 import SubMenu from 'antd/es/menu/SubMenu';
 
 const { Sider } = Layout;
@@ -7,14 +13,20 @@ const { Sider } = Layout;
 const Sidebar: React.FC = () => {
   return (
     <Sider breakpoint="lg" collapsedWidth="80" width={250}>
-      <Menu theme="dark">
-        <Menu.Item key="1">
+      <Menu theme="dark" mode="inline">
+        <Menu.Item key="1" icon={<HomeOutlined />}>
           <a href="/homepage"> Home</a>
         </Menu.Item>
-        <SubMenu key="2" title="Products by category">
+
+        <SubMenu
+          key="2"
+          icon={<AppstoreOutlined />}
+          title="Products by category"
+        >
           <Menu.Item key="2.1">
             <a href="/category/furniture">Furniture Hardware</a>
           </Menu.Item>
+
           <Menu.Item key="2.2">
             <a href="/category/bathroom">Bathroom Hardware</a>
           </Menu.Item>
@@ -25,8 +37,12 @@ const Sidebar: React.FC = () => {
             <a href="/category/floor">Floor Covering</a>
           </Menu.Item>
         </SubMenu>
-        <Menu.Item key="3">Create new quote requests</Menu.Item>
-        <Menu.Item key="4">Quote requests list</Menu.Item>
+        <Menu.Item key="3" icon={<FormOutlined />}>
+          Create new quote requests
+        </Menu.Item>
+        <Menu.Item key="4" icon={<OrderedListOutlined />}>
+          Quote requests list
+        </Menu.Item>
       </Menu>
     </Sider>
   );
