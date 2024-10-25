e88130b4e0808530d2e6e3c2a8a8554f769453ba
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-25 17:07:10 +0200

    feat: Add more features: select the quantity and add to the basket

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Homepage.css';
+import { Button, Card, InputNumber } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 
 export interface ProductProps {
   id: number;
@@ -9,13 +12,49 @@ export interface ProductProps {
   category: string;
 }
 
-const Product: React.FC<ProductProps> = ({ image, name, price }) => {
+const Product: React.FC<ProductProps> = ({ id, image, name, price }) => {
+  const [quantity, setQuantity] = useState<number>(1);
+  const navigate = useNavigate();
+
+  const handleAddToBasket = () => {
+    console.log(`Added ${quantity} of ${name}' to the basket!`);
+  };
+
+  const goToProductDetails = () => {
+    navigate(`/product/${id}`);
+  };
+
+  const handleQuantityChange = (value: number | null) => {
+    if (value != null) {
+      setQuantity(value);
+    }
+  };
   return (
-    <div className="product">
-      <img src={image} alt={name} className="Product-img" />
+    <Card
+      hoverable
+      cover={<img src={image} alt={name} />}
+      className="product"
+      onClick={goToProductDetails}
+    >
       <h3> {name} </h3>
       <p> {price} </p>
-    </div>
+      <div onClick={(e) => e.stopPropagation()}>
+        <InputNumber
+          min={1}
+          value={quantity}
+          onChange={handleQuantityChange}
+          style={{ marginRight: '10px' }}
+        />
+        <Button
+          type="primary"
+          icon={<ShoppingCartOutlined />}
+          onClick={handleAddToBasket}
+        >
+          {' '}
+        </Button>
+      </div>
+    </Card>
   );
 };
+
 export default Product;
