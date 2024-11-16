04ad7112a46dc66a44e6ed0199d342147db5ca5e
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-16 16:56:44 +0100

    fix: Avoid the decplication of selected product in the basket

--- frontend/src/context/BasketContext.tsx
+++ frontend/src/context/BasketContext.tsx
@@ -18,8 +18,20 @@ const BasketProvider: React.FC<{ children: React.ReactNode }> = ({
   >([]);
 
   const addToBasket = (product: ProductInterface, quantity: number) => {
-    setBasket((prevBasket) => [...prevBasket, { product, quantity }]);
-    console.log(basket);
+    setBasket((prevBasket) => {
+      const existingProduct = prevBasket.find(
+        (item) => item.product.id === product.id,
+      );
+      if (existingProduct != undefined) {
+        return prevBasket.map((item) => {
+          return item.product.id === product.id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item;
+        });
+      } else {
+        return [...prevBasket, { product, quantity }];
+      }
+    });
   };
   return (
     <BasketContext.Provider value={{ basket, addToBasket }}>
