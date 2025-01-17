3ade1331d534c15f99618a0c919a1b88923bc194
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-17 10:18:28 +0100

    feat: Post new user using the register form

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,20 +1,72 @@
 import { Button, Form, Input, notification } from "antd";
 import React from "react";
-import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  LockOutlined,
+  MailOutlined,
+  HomeOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
 import "../styles/Register.css";
 import { NavigateFunction } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const navigate: NavigateFunction = useNavigate();
-  const onFinish = () => {
-    notification.success({
-      message: "Création réussie",
-      description: "Login juste now!",
-    });
-    setTimeout(() => {
-      navigate("/login");
-    }, 2000);
+  const addUser = async (values: {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    adress: string;
+    region_code: string;
+    phone_number: string;
+  }) => {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      adress,
+      region_code,
+      phone_number,
+    } = values;
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: password,
+          adress: adress,
+          region_code: region_code,
+          phone_number: phone_number,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to submit the new user!");
+      }
+
+      const data = await response.json();
+
+      notification.success({
+        message: "Création réussie",
+        description: "Login juste now!",
+      });
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+
+      return data;
+    } catch (error) {
+      console.error("Error submitting the new user ", error);
+    }
   };
   return (
     <div className="register-container">
@@ -22,8 +74,32 @@ const Register = () => {
         name="register_form"
         initialValues={{ remember: true }}
         className="register-form"
-        onFinish={onFinish}
+        onFinish={addUser}
       >
+        <Form.Item
+          name="first_name"
+          rules={[
+            { required: true, message: "Please put here your First Name! " },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="First_name"
+            className="site-form-item-icon"
+          />
+        </Form.Item>
+        <Form.Item
+          name="last_name"
+          rules={[
+            { required: true, message: "Please put here your last Name! " },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="Last_name"
+            className="site-form-item-icon"
+          />
+        </Form.Item>
         <Form.Item
           name="email"
           rules={[
@@ -37,18 +113,6 @@ const Register = () => {
           />
         </Form.Item>
         <Form.Item
-          name="username"
-          rules={[
-            { required: true, message: "Please put here your user Name! " },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined />}
-            placeholder="Username"
-            className="site-form-item-icon"
-          />
-        </Form.Item>
-        <Form.Item
           name="password"
           rules={[
             { required: true, message: "Please put here your password! " },
@@ -58,6 +122,32 @@ const Register = () => {
             prefix={<LockOutlined />}
             type="password"
             placeholder="Password"
+            className="site-form-item-icon"
+          />
+        </Form.Item>
+        <Form.Item name="adress">
+          <Input
+            prefix={<HomeOutlined />}
+            placeholder="Adress"
+            className="site-form-item-icon"
+          />
+        </Form.Item>
+        <Form.Item
+          name="region_code"
+          rules={[
+            { required: true, message: "Please put here your Region Code! " },
+          ]}
+        >
+          <Input
+            prefix={<HomeOutlined />}
+            placeholder="Region_code"
+            className="site-form-item-icon"
+          />
+        </Form.Item>
+        <Form.Item name="phone_number">
+          <Input
+            prefix={<PhoneOutlined />}
+            placeholder="Phone_number"
             className="site-form-item-icon"
           />
         </Form.Item>
