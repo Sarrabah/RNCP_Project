a92b716444e8e77ca379d2010116895f2b3a4f20
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-18 15:34:43 +0100

    feat: Implement logout feature

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import artcreapro from "../assets/art-crea-pro.png";
 import { Header } from "antd/es/layout/layout";
 import { Link } from "react-router-dom";
 import { useBasketContext } from "../context/BasketContext";
-import { Badge } from "antd";
+import { Badge, Dropdown, MenuProps, message } from "antd";
 
 const NavBar: React.FC = () => {
   const { basket } = useBasketContext();
@@ -14,6 +14,52 @@ const NavBar: React.FC = () => {
   for (const item of basket) {
     totalBasketItems = totalBasketItems + item.quantity;
   }
+  const getCSRFToken = (): string | null => {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+      if (cookie.startsWith("csrftoken=")) {
+        return cookie.split("=")[1];
+      }
+    }
+    return null;
+  };
+
+  const handleLogout = async (): Promise<any> => {
+    try {
+      const csrfToken = getCSRFToken();
+      if (!csrfToken) {
+        throw new Error("CSRF token not found. Please refresh the page.");
+      }
+
+      const response = await fetch("/api/logout", {
+        method: "POST",
+        credentials: "include", // Include session cookies
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": csrfToken, // Include the CSRF token
+        },
+      });
+      if (response.ok) {
+        message.success("Logged out successfully!");
+        window.location.href = "/login";
+      } else {
+        message.error("Failed to logout!");
+      }
+    } catch (error) {
+      message.error("An error is occured while logging out!");
+      console.error("Logout error: ", error);
+    }
+  };
+  const profileMenu: MenuProps["items"] = [
+    {
+      key: "profile",
+      label: <Link to="/profile">Profile</Link>,
+    },
+    {
+      key: "logout",
+      label: <span onClick={handleLogout}>Logout</span>,
+    },
+  ];
 
   return (
     <Header className="navbar">
@@ -22,9 +68,13 @@ const NavBar: React.FC = () => {
       </Link>
       <SearchBar />
       <div className="icons">
-        <Link to="/profile">
+        <Dropdown
+          menu={{ items: profileMenu }}
+          placement="bottomRight"
+          trigger={["hover"]}
+        >
           <UserOutlined />
-        </Link>
+        </Dropdown>
         <Link to="/basket">
           <Badge count={totalBasketItems} style={{ backgroundColor: "black" }}>
             <ShoppingCartOutlined style={{ fontSize: "20px" }} />
