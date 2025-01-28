aa9e9558753aa6699394930d8f47c93ca9bb49db
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-28 07:39:32 +0100

    feat: Dockerise all the app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("my_app.urls")),
-    path("", TemplateView.as_view(template_name="index.html")),
+    # path("", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
