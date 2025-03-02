c8b6a30e371d61dcd0f4b5e164a7136dbd462d09
sarrabah <sarrabenarbia1996@gmail.com>
2025-03-02 15:37:36 +0100

    test: Typing

--- frontend/src/pages/Register/Register.test.tsx
+++ frontend/src/pages/Register/Register.test.tsx
@@ -2,11 +2,11 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Register from "./RegisterPage";
 
-const expectErrorMessage = (message: RegExp) => {
+const expectErrorMessage = (message: string) => {
   return waitFor(() => expect(screen.getByText(message)).toBeInTheDocument());
 };
 
-const expectNoErrorMessage = (message: RegExp) => {
+const expectNoErrorMessage = (message: string) => {
   return waitFor(() =>
     expect(screen.queryByText(message)).not.toBeInTheDocument(),
   );
@@ -26,12 +26,12 @@ test("Form validation: errors appear and disappear correctly", async () => {
   });
 
   fireEvent.click(submitButton);
-  await expectErrorMessage(/Please put here your adress mail!/i);
-  await expectErrorMessage(/Please put here your password!/i);
+  await expectErrorMessage("Please put here your adress mail!");
+  await expectErrorMessage("Please put here your password!");
 
   fireEvent.change(emailInput, { target: { value: "test@gmail.com" } });
   fireEvent.change(passwordInput, { target: { value: "helloSecure3#" } });
   fireEvent.click(submitButton);
-  await expectNoErrorMessage(/Please put here your adress mail!/i);
-  await expectNoErrorMessage(/Please put here your password!/i);
+  await expectNoErrorMessage("Please put here your adress mail!");
+  await expectNoErrorMessage("Please put here your password!");
 });
