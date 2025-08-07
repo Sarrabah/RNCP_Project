069fbca5db32272a71bd6114f133cd1ab10b5b28
sarrabah <sarrabenarbia1996@gmail.com>
2025-08-07 16:08:23 +0200

    feat: Add forms

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -38,3 +38,19 @@ export interface ProductDetails {
   product_image: string;
   quantity: number;
 }
+
+export interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+export interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+export interface SignFormsProps {
+  activeForm: "signin" | "signup";
+  onFormChange: (form: "signin" | "signup" | null) => void;
+  onCloseForm: () => void;
+}
