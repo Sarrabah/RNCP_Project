38e693f21bd76d757ad8590119f5b6f6939e3851
sarrabah <sarrabenarbia1996@gmail.com>
2025-11-27 15:18:36 +0100

    test: add a failed test when the password is empty

--- backend/my_app/unittests/test_user_creation.py
+++ backend/my_app/unittests/test_user_creation.py
@@ -18,3 +18,46 @@ class UserTestCase(TestCase):
             }
         )
         self.assertEqual(type(user), Architect)
+
+    def test_if_the_password_is_void(self):
+        self.assertRaises(
+            ValueError,
+            create_new_user,
+            {
+                "first_name": "Tata",
+                "last_name": "Taa",
+                "email": "tata@gmail.com",
+                "password": "",
+                "adress": "123 Main St",
+                "region_code": "1",
+                "phone_number": "1234567890",
+            },
+        )
+
+    def test_if_the_password_is_hashed(self):
+        user = create_new_user(
+            {
+                "first_name": "Tata",
+                "last_name": "Taa",
+                "email": "tata@gmail.com",
+                "password": "password123",
+                "adress": "123 Main St",
+                "region_code": "1",
+                "phone_number": "1234567890",
+            }
+        )
+        self.assertNotEqual(user.password, "password123")
+
+    def test_if_the_hashed_password_is_not_empty(self):
+        user = create_new_user(
+            {
+                "first_name": "Tata",
+                "last_name": "Taa",
+                "email": "tata@gmail.com",
+                "password": "password123",
+                "adress": "123 Main St",
+                "region_code": "1",
+                "phone_number": "1234567890",
+            }
+        )
+        self.assertNotEqual(len(user.password), 0)
