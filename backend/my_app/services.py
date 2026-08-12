cd6a3a9754a7e5c8b50a7496f794c0e1dda6fea5
sarrabah <sarrabenarbia1996@gmail.com>
2026-08-12 20:31:38 +0200

    fix: Verify the right user

--- backend/my_app/services.py
+++ backend/my_app/services.py
@@ -49,9 +49,10 @@ def get_product_details(id):
     return productDetails
 
 
-def create_basket_elements(valid_data):
+def create_basket_elements(valid_data, user_id):
     for qr_id in valid_data["quoteRequestIdList"]:
-
+        if not QuoteRequest.objects.filter(pk=qr_id, archi_id=user_id).exists():
+            return None
         for p in valid_data["productInformations"]:
             instanceQuoteRequest = QuoteRequest.objects.get(pk=qr_id)
             instanceProduct = Product.objects.get(pk=p["id"])
